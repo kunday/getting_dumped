72286a1995ede003c4b951afe8c05c8c5c7880bb
Arvind Kunday <hi@kunday.com>
2011-08-31 22:39:09 +1000

    initial commit

---
+++ getting_dumped.gemspec
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name        = "getting_dumped"
+  s.version     = '0.0.6'
+  s.platform    = Gem::Platform::RUBY
+  s.authors     = ["Arvind Kunday"]
+  s.email       = ["hi@kunday.com"]
+  s.homepage    = "https://github.com/kunday/getting_dumped"
+  s.summary     = %q{the historical RSpec run formatter}
+  s.description = %q{the historical RSpec run formatter}
+
+  s.rubyforge_project = "getting_dumped"
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  s.add_dependency('rspec', ["~> 2.0"])
+  s.add_dependency('rake')
+  s.add_dependency('data_mapper')
+  s.add_dependency('dm-sqlite-adapter')
+end
---
+++ lib/getting_dumped.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require 'rspec/core/formatters/base_formatter'
+
+class GettingDumped < RSpec::Core::Formatters::BaseFormatter
+  def initialize(options)
+    super
+    @time = nil
+    @start_time = Time.now
+    @example_times = []
+  end
+
+  def start(count)
+    p "Running #{count} examples"
+  end
+
+  def example_started(example)
+    @time = Time.now
+    super
+  end
+  def example_passed(example)
+    super
+    @example_times << [example.full_description, Time.now - @time]
+  end
+
+  def start_dump
+    super
+    p "Total Time: #{Time.now - @time}"
+
+    p "Dumping Results. This could take a moment."
+
+    run = Run.new(:run_at => Time.now, :duration => Time.now - @start_time)
+    run.save
+
+    @example_times.each do |example|
+      Example.new(:name => example[0], :run_time => example[1], :run_id => run.id).save
+    end
+
+    p "Dumping complete"
+  end
+end
