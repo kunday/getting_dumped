e33274fc2b538d9d6801181da5e67450f25acad1
Arvind Kunday <hi@kunday.com>
2011-09-04 22:44:08 +1000

    Added a view for examples

--- lib/getting_dumped/app.rb
+++ lib/getting_dumped/app.rb
@@ -1,7 +1,12 @@
 require "sinatra"
-require "models"
+require "getting_dumped"
 
 get '/runs' do
   @runs = Run.all
   erb :runs
+end
+
+get '/run/:id' do
+  @examples = Run.find(params[:id]).first.examples
+  erb :examples
 end
